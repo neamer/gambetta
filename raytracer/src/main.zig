d9e08118cbe73a73efdf91a45d48e0823ac9eb87
Amer Neziric <ne_amer@hotmail.com>
2026-08-26 01:12:46 +0200

    feat(ray): build out canvas abstraction as per book introduction

--- raytracer/src/main.zig
+++ raytracer/src/main.zig
@@ -1,53 +1,73 @@
+const std = @import("std");
 const rl = @import("raylib");
 
+const screen_width = 800;
+const screen_height = 450;
+
+const Canvas = struct {
+    texture: rl.Texture2D,
+    pixels: [screen_width * screen_height]rl.Color = @splat(rl.Color.white),
+
+    pub const Point = struct {
+        x: usize,
+        y: usize,
+    };
+
+    fn init() !Canvas {
+        const img = rl.genImageColor(screen_width, screen_height, .black);
+        const texture = try rl.loadTextureFromImage(img);
+        rl.unloadImage(img);
+
+        return Canvas {
+            .texture = texture,
+        };
+    }
+
+    fn deinit(self: *Canvas) void {
+        rl.unloadTexture(self.texture);
+    }
+
+    fn pixel(point: Point) usize {
+        if (point.x < 0 or point.y < 0) return 0;
+
+        return point.y * screen_width + point.x;
+    }
+
+    fn screen(x: i32, y: i32) Point {
+        return Point {
+            .x = @intCast(std.math.clamp(screen_width / 2 + x, 0, screen_width)),
+            .y = @intCast(std.math.clamp(screen_height / 2 - y, 0, screen_height)),
+        };
+    }
+
+    fn putPixel(self: *Canvas, x: i32, y: i32, color: rl.Color) void {
+        self.pixels[pixel(screen(x, y))] = color;
+    }
+
+    fn draw(self: *Canvas) void {
+        rl.updateTexture(self.texture, &self.pixels);
+        rl.drawTexture(self.texture, 0, 0, .white);
+    }
+};
+
 pub fn main() anyerror!void {
-
-    const screen_width = 800;
-    const screen_height = 450;
-
     rl.initWindow(screen_width, screen_height, "Raytracer");
     defer rl.closeWindow();
 
-    rl.setTargetFPS(360);
+    rl.setTargetFPS(60);
 
-    const img = rl.genImageColor(screen_width, screen_height, .black);
-    const tex = try rl.loadTextureFromImage(img);
-    rl.unloadImage(img);
-    defer rl.unloadTexture(tex);
+    var canvas: Canvas = try .init();
+    defer canvas.deinit();
 
-    var pixels: [screen_width * screen_height]rl.Color = @splat(rl.Color.white);
-
-    var idx: usize = 0;
-    var override_color: rl.Color = .blue;
-    const stroke = 50;
+    canvas.putPixel(-200, -112, .red);
 
     while (!rl.windowShouldClose()) {
-
-        fillColumn(&pixels, idx, screen_width, stroke, override_color);
-
-        idx += 1;
-
-        if (idx % screen_width == 0) {
-            if (idx < pixels.len - (stroke - 1) * screen_width) {
-                idx += screen_width * (stroke - 1);
-            } else {
-                idx = 0;
-                override_color = .red;
-            }
-        }
-
-        rl.updateTexture(tex, &pixels);
 
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(.dark_gray);
-        rl.drawTexture(tex, 0, 0, .white);
+        canvas.draw();
     }
 }
 
-fn fillColumn(pixels: []rl.Color, idx: usize, screen_width: usize, comptime stroke: usize, color: rl.Color) void {
-    inline for (0..stroke) |i| {
-        pixels[idx + screen_width * i] = color;
-    }
-}
